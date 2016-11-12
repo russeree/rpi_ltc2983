29cf47425d00778391075bb55d0ed6b34c42cd79
russeree <reese@rfpga.com>
2016-11-12 11:14:03 +0000

    Created a TCP Node Client to connect with LTC2983 server

--- node_serv/node_serv.js
+++ node_serv/node_serv.js
@@ -1,13 +1,36 @@
-console.log("I loaded something :D \n");
+// Magiks Genetation
+var port = 9777;
+var serv_ip = '127.0.0.1';
+var conn_count = 0;
 
 // Load the HTTP module to create a server
-var http = require("http");
+var http = require('http');
+var net = require('net');
 
-console.log("Init Complete");
+console.log("Welcome to the LTC2983 themocouple management System.");
 // Configure server to respong with hello-world for all requests
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World\n");
+    response.end("Hello World\n" + conn_count);
+});
+
+// Create a new socket server to connect to the rPI thermocouple server
+var thermo_client = new net.Socket();
+thermo_client.connect(port, serv_ip, function() {
+    console.log('Connected to RPI Thermocontroller Server');
+    conn_count++;
+    thermo_client.write('Init');
+});
+
+// On any recived client data print the message onto the screen 
+thermo_client.on('data', function(data) {
+    console.log('Received ' + data);
+    thermo_client.destroy();
+});
+
+// On client close print that the client has closed
+thermo_client.on('close', function(){
+    console.log('Connection Closed'); 
 });
 
 // Listen on port 8000, IP Defaults 127.0.0.1
