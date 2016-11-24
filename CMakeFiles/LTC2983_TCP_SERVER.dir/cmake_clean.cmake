78c2459d80c40983784d9c3e28112871bc8fdb42
russeree <reese@rfpga.com>
2016-11-24 11:41:39 +0000

    cmake added to project

---
+++ CMakeFiles/LTC2983_TCP_SERVER.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LTC2983_TCP_SERVER.dir/src/main.cc.o"
+  "CMakeFiles/LTC2983_TCP_SERVER.dir/src/server.cc.o"
+  "CMakeFiles/LTC2983_TCP_SERVER.dir/src/ltc2983.cc.o"
+  "LTC2983_TCP_SERVER.pdb"
+  "LTC2983_TCP_SERVER"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LTC2983_TCP_SERVER.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/ltc2983_serv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ltc2983_serv.dir/main.cc.o"
+  "ltc2983_serv.pdb"
+  "ltc2983_serv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ltc2983_serv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
