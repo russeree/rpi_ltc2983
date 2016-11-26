b80ea4e0257ae6277a8c934ee55db3d5f07c1a87
russeree <reese@rfpga.com>
2016-11-26 10:13:34 +0000

    Began server application development: Compiles

--- include/server.h
+++ include/server.h
@@ -1,15 +1,24 @@
 #ifndef SERVER
 #define SERVER
 
+//Boost includes needed for classs types
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
 
+//Option to manualy define port
+#define _PORT 9777
+
 using boost::asio::ip::tcp;
 
-extern boost::asio::io_service ioservice; 
+extern unsigned int get_port_input(void);
 
-class session
+class tcp_server
 {
 public:
+    tcp_server(boost::asio::io_service& io_service);
+private:
+    tcp::acceptor acceptor_;
 };
 #endif
--- src/server.cc
+++ src/server.cc
@@ -5,3 +5,30 @@
 // Server Specific Libraries
 #include "server.h"
 
+extern unsigned int get_port_input(void)
+{
+    unsigned int port;
+#ifdef _PORT
+    port = _PORT;
+#else
+    std::cout << "Enter a port number for the TCP server ";
+    std::cin  >> port;
+    try
+    {
+        if(port > 65535)
+            throw port;
+    }
+    catch(unsigned int port)
+    {
+        std::cout << "Port " << port << " is invalid. Port set to 0";
+        port = 0;
+    }
+#endif 
+    return port;
+};
+
+
+tcp_server::tcp_server(boost::asio::io_service& io_service) : acceptor_(io_service, tcp::endpoint(tcp::v4(), get_port_input()))
+{
+};
+
