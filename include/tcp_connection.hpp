66d34fe13e75cd18b451d292417d680d7d95e9bd
russeree <reese@rfpga.com>
2016-11-27 06:48:41 +0000

    Finished up the new connection handler.

--- include/tcp_connection.hpp
+++ include/tcp_connection.hpp
@@ -12,37 +12,14 @@ class tcp_connection
   : public boost::enable_shared_from_this<tcp_connection>
 {
 public:
-  typedef boost::shared_ptr<tcp_connection> pointer;
-
-  static pointer create(boost::asio::io_service& io_service);
-
-  tcp::socket& socket()
-  {
-    return socket_;
-  }
-
-  void start()
-  {
-    message_ = "TEST";
-
-    boost::asio::async_write(socket_, boost::asio::buffer(message_),
-        boost::bind(&tcp_connection::handle_write, shared_from_this(),
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));
-  }
-
+    typedef boost::shared_ptr<tcp_connection> pointer;
+    static pointer create(boost::asio::io_service& io_service);
+    tcp::socket& socket(void);
+    void start();
 private:
-  tcp_connection(boost::asio::io_service& io_service)
-    : socket_(io_service)
-  {
-  }
-
-  void handle_write(const boost::system::error_code& /*error*/,
-      size_t /*bytes_transferred*/)
-  {
-  }
-
-  tcp::socket socket_;
-  std::string message_;
+    tcp_connection(boost::asio::io_service& io_service);
+    void handle_write(const boost::system::error_code& ,size_t);
+    tcp::socket socket_;
+    std::string message_;
 };
 #endif
--- src/tcp_connection.cc
+++ src/tcp_connection.cc
@@ -7,7 +7,34 @@
 // This will include all the boost libs needed for this class
 #include <tcp_connection.hpp>
 
+// Create a pointer to a new TCP connection
 tcp_connection::pointer tcp_connection::create(boost::asio::io_service& io_service)
 {
     return pointer(new tcp_connection(io_service));
+}
+
+// Return the private socket varible
+tcp::socket& tcp_connection::socket(void)
+{
+    return socket_;
+}
+
+// The task to be completed upon a connection
+void tcp_connection::start()
+{
+    message_ = "TEST";
+    boost::asio::async_write(socket_, boost::asio::buffer(message_),
+        boost::bind(&tcp_connection::handle_write, shared_from_this(),
+        boost::asio::placeholders::error,
+        boost::asio::placeholders::bytes_transferred));
+}
+
+// Class Constructor
+tcp_connection::tcp_connection(boost::asio::io_service& io_service) : socket_(io_service)
+{
+}
+
+// Connection handler
+void tcp_connection::handle_write(const boost::system::error_code&, size_t)
+{
 }
