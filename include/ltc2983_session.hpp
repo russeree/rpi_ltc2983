c2c8f589ad593e23f07821a9c41cb0531c4171f6
russeree <reese@rfpga.com>
2016-11-29 12:54:15 +0000

    Doc'd classes

--- include/ltc2983_session.hpp
+++ include/ltc2983_session.hpp
@@ -4,7 +4,15 @@
 #include <ltc2983.hpp>
 
 using boost::asio::ip::tcp;
+//Use in session constructor to obtain a port if one is not defined
+extern unsigned int get_port_input(void);
 
+/**
+ * @desc: for now this represent a LTC2983 session, a session can have many clients but idealy only
+ * one session per LTC2983 should exist. The session acts as a TCP server
+ * @param: [ltc2983&] A reference to an ltc2983 class object that has been constructed.
+ * @param: [io_service] A reference to an ASIO io service to communicate over. 
+ **/
 class ltc2983_session :
     public std::enable_shared_from_this<ltc2983_session>
 {
@@ -12,7 +20,7 @@ public:
     ltc2983_session(ltc2983& dev, boost::asio::io_service& io_service);
     virtual ~ltc2983_session();
 private:
-    // Pointer to the ltc2983 to be used during this session
     tcp::acceptor acceptor_;
-    ltc2983* dev_; 
+    //Reference to the ltc2983 class, !!NO CPY/NEW!!
+    ltc2983& dev_; 
 };
