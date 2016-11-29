5496efaf11fd03816aecbf5e0fb354bf010719a3
russeree <reese@rfpga.com>
2016-11-29 12:17:26 +0000

    Began work on session implementation.

--- include/ltc2983_session.hpp
+++ include/ltc2983_session.hpp
@@ -1,14 +1,18 @@
 #include <iostream>
-#include <memory> 
+#include <memory>
+#include <boost/asio.hpp>
 #include <ltc2983.hpp>
+
+using boost::asio::ip::tcp;
 
 class ltc2983_session :
     public std::enable_shared_from_this<ltc2983_session>
 {
 public:
-    ltc2983_session();
+    ltc2983_session(ltc2983& dev, boost::asio::io_service& io_service);
     virtual ~ltc2983_session();
 private:
     // Pointer to the ltc2983 to be used during this session
-    ltc2983* dev; 
-}
+    tcp::acceptor acceptor_;
+    ltc2983* dev_; 
+};
