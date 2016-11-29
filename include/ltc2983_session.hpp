dd3a514e6c756ee363fe5e73df5d79b9ee5de993
russeree <reese@rfpga.com>
2016-11-29 14:43:22 +0000

    Save: Added a start function to the session server.

--- include/ltc2983_session.hpp
+++ include/ltc2983_session.hpp
@@ -3,10 +3,15 @@
 #include <boost/asio.hpp>
 #include <ltc2983.hpp>
 
+//New connection handler 
+#include <tcp_connection.hpp>
+
+#define _PORT 9777
+#define _VERBOSE
+
 using boost::asio::ip::tcp;
 //Use in session constructor to obtain a port if one is not defined
 extern unsigned int get_port_input(void);
-
 /**
  * @desc: for now this represent a LTC2983 session, a session can have many clients but idealy only
  * one session per LTC2983 should exist. The session acts as a TCP server
@@ -19,6 +24,7 @@ class ltc2983_session :
 public:
     ltc2983_session(ltc2983& dev, boost::asio::io_service& io_service);
     virtual ~ltc2983_session();
+    void start_accept();
 private:
     tcp::acceptor acceptor_;
     //Reference to the ltc2983 class, !!NO CPY/NEW!!
--- src/ltc2983_session.cc
+++ src/ltc2983_session.cc
@@ -3,7 +3,6 @@
  * @desc: A TCP session for the LTC2983
  **/
 #include <ltc2983_session.hpp>
-
 /**
  * @desc: is there is no port defined for the TCP server, prompt the user to enter one.
  * @param: No parameters
@@ -29,7 +28,6 @@ extern unsigned int get_port_input(void)
 #endif
     return port;
 }
-
 /**
  * @desc: LTC2983 TCP session constructor
  * @param: [ltc2983*] pointer a a ltc2983 device class object
@@ -37,6 +35,7 @@ extern unsigned int get_port_input(void)
 ltc2983_session::ltc2983_session(ltc2983& dev, boost::asio::io_service& io_service) :
     dev_(dev), acceptor_(io_service, tcp::endpoint(tcp::v4(), get_port_input()))
 {
+    start_accept(); //Start accepting connections on the current endpoint;
 }
 
 /**
@@ -44,5 +43,14 @@ ltc2983_session::ltc2983_session(ltc2983& dev, boost::asio::io_service& io_servi
  **/
 ltc2983_session::~ltc2983_session(void)
 {
+#ifdef _VERBOSE
+    std::cout << "Destroyed a LTC2983 session";
+#endif
 }
-    
+
+/**
+ * @desc: Starts an individual TCP session 
+ **/ 
+void ltc2983_session::start_accept()
+{
+}
